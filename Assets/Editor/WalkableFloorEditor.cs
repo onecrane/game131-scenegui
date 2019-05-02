e5cb750b3fe7880a38e2be38c7d16bb344ec8500
svc-student <svc-student@student.directory.jccc>
2019-05-02 17:39:38 -0500

    Initial commit

---
+++ Assets/Editor/WalkableFloorEditor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WalkableFloor))]
+public class WalkableFloorEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.LabelField("Hello");
+    }
+
+    private void OnSceneGUI()
+    {
+        // Handle mouse clicks
+        Event evt = Event.current;
+
+        switch (evt.type)
+        {
+            case EventType.MouseDown:
+                if (evt.button == 0)
+                {
+                    if (HandleMouseDown(evt.mousePosition)) evt.Use();
+                }
+                break;
+            case EventType.KeyDown:
+                Debug.Log("Key down: " + evt.keyCode);
+                break;
+            case EventType.KeyUp:
+                Debug.Log("Key up: " + evt.keyCode);
+                break;
+        }
+    }
+
+    bool HandleMouseDown(Vector2 mousePosition)
+    {
+        // Did I get the path or a waypoint?
+        // Create first.
+
+        // Find what you clicked on:
+        RaycastHit info;
+        if (Physics.Raycast(HandleUtility.GUIPointToWorldRay(mousePosition),out info ))
+        {
+            if (info.collider.gameObject.GetComponent<WalkableFloor>() == this.target as WalkableFloor)
+            {
+                // Hit the floor; create a Waypoint
+                GameObject waypointTemplate = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Waypoint.prefab", typeof(GameObject)) as GameObject;
+                GameObject newWaypoint = GameObject.Instantiate(waypointTemplate);
+                newWaypoint.transform.position = info.point;
+
+                return true;
+            }
+
+            if (info.transform.parent != null && info.transform.parent.gameObject.tag == "Waypoint")
+            {
+                Debug.Log("Clicked a waypoint");
+                // Select that waypoint
+                Selection.activeObject = info.transform.parent.gameObject;
+                return true;
+            }
+            Debug.Log("Clicked something with tag " + info.collider.gameObject.tag);
+
+        }
+        return false;
+
+    }
+
+}
---
+++ Assets/Editor/WaypointEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Waypoint))]
+public class WaypointEditor : Editor
+{
+
+    private void OnSceneGUI()
+    {
+        Event evt = Event.current;
+
+        switch (evt.type)
+        {
+            case EventType.MouseDrag:
+                if (evt.button == 0)
+                {
+                    if (HandleDrag(evt)) evt.Use();
+                }
+                break;
+            case EventType.MouseUp:
+                if (evt.button == 0)
+                {
+                    if (HandleMouseUp(evt)) evt.Use();
+                }
+                break;
+            case EventType.MouseDown:
+                if (evt.button == 0)
+                {
+                    if (HandleMouseDown(evt)) evt.Use();
+                }
+                break;
+        }
+    }
+
+    private bool HandleDrag(Event evt)
+    {
+        // Move with the mouse:
+        // Move to the current mouse position if the current mouse position is on a suitable area. Borrow from previous click handler.
+        // Bear in mind that we only want to know about the walkable floor.
+        // Probably the simplest way we can implement that is with layers.
+
+        RaycastHit info;
+        if (Physics.Raycast(HandleUtility.GUIPointToWorldRay(evt.mousePosition), out info, float.MaxValue, 1 << LayerMask.NameToLayer("WalkableFloor")))
+        {
+            // Hit the floor; move the selected object (this) to this location
+            (target as Waypoint).transform.position = info.point;
+            return true;
+        }
+        return false;
+    }
+
+    private bool HandleMouseUp(Event evt)
+    {
+        return false;
+    }
+
+    private bool HandleMouseDown(Event evt)
+    {
+        return false;
+    }
+
+}
